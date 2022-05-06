ea73b501581fad7ce06645f78fa3ca0ad7ae3413
shirokuma0503 <aika.onimura@gmail.com>
2022-05-06 14:20:32 +0900

    9_120:v-model

--- udemy-vue.js/src/App.vue
+++ udemy-vue.js/src/App.vue
@@ -1,31 +1,26 @@
 <template>
   <div>
-    <GoodHeader>
-      <template>
-        <h1>slotで表示するヘッダー</h1>
-      </template>
-    </GoodHeader>
+    <div>
+      <h2>インベントのフォーム</h2>
+      <label for="title">タイトル</label>
+      <input
+        id="title"
+        type="text"
+        v-model="eventData.title"
+      >
 
-    <button @click="currentComponent = 'TestHome'">TestHome</button>
-    <button @click="currentComponent = 'TestAbout'">TestAbout</button>
-    <keep-alive>
-      <component :is="currentComponent"></component>
-    </keep-alive>
+      <p>{{ eventData.title }}</p>
+    </div>
   </div>
 </template>
 
 <script>
-import GoodHeader from './components/GoodHeader.vue';
-import TestHome from './components/TestHome.vue';
-
 export default {
-  components: {
-    GoodHeader: GoodHeader,
-    TestHome: TestHome
-  },
   data() {
     return {
-      currentComponent: "TestHome"
+      eventData: {
+        title: "タイトル"
+      }
     }
   }
 };
