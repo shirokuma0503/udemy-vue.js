1c6931163dafc55f9ef49afb2af8aa834cc3b2ce
shirokuma0503 <aika.onimura@gmail.com>
2022-05-09 15:03:24 +0900

    10_142:カスタムディレクティブのローカル登録

--- udemy-vue.js/src/components/TestHome.vue
+++ udemy-vue.js/src/components/TestHome.vue
@@ -1,6 +1,21 @@
 <template>
   <div>
-    <p v-border:solid.round.shadow="{width: '5px', color: 'brown'}">Home.vueです</p>
+    <p v-border:solid.round="{width: '5px', color: 'brown'}">Home.vueです</p>
     <input>
   </div>
 </template>
+
+<script>
+export default {
+  directives: {
+    border(el, binding) {
+      el.style.borderWidth = binding.value.width;
+      el.style.borderColor = binding.value.color;
+      el.style.borderStyle = binding.arg;
+      if (binding.modifiers.round) {
+        el.style.borderRadius = "1rem";
+      }
+        }
+      }
+}
+</script>
--- udemy-vue.js/src/main.js
+++ udemy-vue.js/src/main.js
@@ -3,14 +3,15 @@ import App from './App.vue';
 import GoodNumber from './components/GoodNumber.vue';
 
 Vue.component('GoodNumber', GoodNumber);
-Vue.directive('border', function(el, binding) {
-  el.style.borderWidth = binding.value.width;
-  el.style.borderColor = binding.value.color;
-  el.style.borderStyle = binding.arg;
-  if (binding.modifiers.round) {
-    el.style.borderRadius = "1rem";
-  }
-});
+// カスタムディレクティブの登録
+// Vue.directive('border', function(el, binding) {
+//   el.style.borderWidth = binding.value.width;
+//   el.style.borderColor = binding.value.color;
+//   el.style.borderStyle = binding.arg;
+//   if (binding.modifiers.round) {
+//     el.style.borderRadius = "1rem";
+//   }
+// });
 
 new Vue({
   render: h => h(App),
