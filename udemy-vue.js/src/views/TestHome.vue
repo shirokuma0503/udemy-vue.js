f6100f3d37d7803d0b577ff81e210ee44e767e3a
shirokuma0503 <aika.onimura@gmail.com>
2022-05-13 21:58:12 +0900

    13_190:それぞれのコンポーネントからURLを切り替える

--- udemy-vue.js/src/views/TestHome.vue
+++ udemy-vue.js/src/views/TestHome.vue
@@ -1,3 +1,16 @@
 <template>
-  <h1>TestHome</h1>
+  <div>
+    <h1>TestHome</h1>
+    <button @click="toTestUsers">TestUsersページへ</button>
+  </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    toTestUsers() {
+      this.$router.push("TestUsers");
+    }
+  }
+}
+</script>
--- udemy-vue.js/src/views/TestUsers.vue
+++ udemy-vue.js/src/views/TestUsers.vue
@@ -1,3 +1,16 @@
 <template>
-  <h1>TestUsers</h1>
+  <div>
+    <h1>TestUsers</h1>
+    <button @click="toTestHome">TestHomeページへ</button>
+  </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    toTestHome() {
+      this.$router.push("TestHome");
+    }
+  }
+}
+</script>
