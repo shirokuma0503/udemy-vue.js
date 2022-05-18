ecceb848bf5129865e20ffeffcc68c52e8f639a9
shirokuma0503 <aika.onimura@gmail.com>
2022-05-18 10:00:06 +0900

    14_226:名前空間

--- udemy-vue.js/src/views/TestHome.vue
+++ udemy-vue.js/src/views/TestHome.vue
@@ -13,7 +13,7 @@ import { mapGetters } from 'vuex';
 
 export default {
   computed: {
-    ...mapGetters(["doubleCount"]),
+    ...mapGetters("count", ["doubleCount"]),
     // message() {
     //   return this.$store.getters.message;
     // }
